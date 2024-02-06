bb9a2385c0e3d6a0b76d040fabd9a8ba180d4d37
danielorangeviana <danieloviana@gmail.com>
2024-02-06 20:30:57 -0300

    Created project, User entity and resource

---
+++ src/main/java/com/br/saraweb20/entities/User.java
@@ -0,0 +1,22 @@
+package com.br.saraweb20.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String login;
+	private String password;
+}
---
+++ src/main/java/com/br/saraweb20/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.br.saraweb20.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.saraweb20.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "orangedwcov", "123");
+		return ResponseEntity.ok().body(user);
+	}
+}
