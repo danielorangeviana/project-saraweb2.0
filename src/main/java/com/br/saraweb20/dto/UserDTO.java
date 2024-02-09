6c37e821c6d23cf580a6add21ecb5a5378bf82ea
danielorangeviana <danieloviana@gmail.com>
2024-02-09 15:18:12 -0300

    Using DTO pattern to return users

---
+++ src/main/java/com/br/saraweb20/dto/UserDTO.java
@@ -0,0 +1,26 @@
+package com.br.saraweb20.dto;
+
+import java.io.Serializable;
+
+import com.br.saraweb20.entities.User;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class UserDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String login;
+	
+	public UserDTO(User obj) {
+		id = obj.getId();
+		login = obj.getLogin();
+	}
+}
--- src/main/java/com/br/saraweb20/resources/UserResource.java
+++ src/main/java/com/br/saraweb20/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.br.saraweb20.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.saraweb20.dto.UserDTO;
 import com.br.saraweb20.entities.User;
 import com.br.saraweb20.service.UserService;
 
@@ -21,20 +23,22 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
+	public ResponseEntity<List<UserDTO>> findAll() {
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User user = service.findById(id);
-		return ResponseEntity.ok().body(user);
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
 	
 	@GetMapping(value = "findByLogin")
-	public ResponseEntity<List<User>> findByLogin(@RequestParam(name = "login") String login) {
+	public ResponseEntity<List<UserDTO>> findByLogin(@RequestParam(name = "login") String login) {
 		List<User> list = service.findByLogin(login.trim().toUpperCase());
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
